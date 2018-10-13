b33367eaa3864040e3e4dcae33889e3d5deb2bf7
ENvironmentSet <sbshw1@gmail.com>
2018-10-13 16:46:10 +0900

    add CircleImage component

---
+++ src/components/atoms/CircleImage/index.js
@@ -0,0 +1,12 @@
+import glamorous from 'glamorous';
+
+const CircleImage = glamorous.img({
+  display: 'block',
+
+  width: '77px',
+  height: '77px',
+
+  borderRadius: '50%',
+});
+
+export default CircleImage;
