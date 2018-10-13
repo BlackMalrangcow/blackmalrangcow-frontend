d589426cd2c31ce3cd2906609339225f26378c28
ENvironmentSet <sbshw1@gmail.com>
2018-10-13 19:09:27 +0900

    Add CardTitle component

---
+++ src/components/atoms/CardTitle/index.js
@@ -0,0 +1,9 @@
+import glamorous from 'glamorous';
+import { CardTitle as BaseCardTitle } from 'reactstrap';
+
+const CardTitle = glamorous(BaseCardTitle)({
+  width: '427px',
+  height: '53px',
+});
+
+export default CardTitle;
