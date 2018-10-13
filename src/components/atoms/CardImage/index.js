fde222f81491ddf459e480d89d9cbd62591b6ed8
ENvironmentSet <sbshw1@gmail.com>
2018-10-13 18:50:28 +0900

    Add CardImage

---
+++ src/components/atoms/CardImage/index.js
@@ -0,0 +1,9 @@
+import glamorous from 'glamorous';
+import { CardImg } from 'reactstrap';
+
+const CardImage = glamorous(CardImg)({
+  width: '427px',
+  height: '267px',
+});
+
+export default CardImage;
